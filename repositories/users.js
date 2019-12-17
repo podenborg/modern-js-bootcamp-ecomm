1de499c344f910b120a6f5f19ebe8d8d93609621
Patrick Odenborg <patrick.odenborg@pearson.com>
2019-12-17 14:45:31 -0600

    Add sign up form validation

--- repositories/users.js
+++ repositories/users.js
@@ -80,11 +80,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-  
-  const user = await repo.getOneBy({ email: 'test@test.com', password: 'mypassword' });
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
