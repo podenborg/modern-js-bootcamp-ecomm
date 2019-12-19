c9c3dcfed644f4fdcf0e434791dae99d98bbc60f
Patrick Odenborg <patrick.odenborg@pearson.com>
2019-12-19 11:56:00 -0600

    Refactor project directory and break up auth routes to separate file

--- index.js
+++ index.js
@@ -1,87 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const usersRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
+// use middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   keys: ['alsk2djfas43dio2wqd']
 }));
 
-app.get('/signup', (req, res) => {
-  res.send(`
-    <div>
-      Your ID is: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email is use.');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match.');
-  }
-
-  // Create a user in our user repo
-  const user = await usersRepo.create({ email, password });
-
-  // Store the id of that user inside the user's cookie
-  req.session.userId = user.id;
-
-  res.send('Account created!');
-});
-
-app.get('/signout', (req, res) => {
-  req.session = null;
-  res.send('You are logged out!');
-});
-
-app.get('/signin', (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({ email });
-
-  if (!user) {
-    return res.send('Email not found.');
-  }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send('Invalid password.');
-  }
-
-  req.session.userId = user.id;
-  res.send('You are signed in!');
-});
+// use route handlers
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log('Listing on port 3000.');
