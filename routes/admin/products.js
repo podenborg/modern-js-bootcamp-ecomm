f37749556110674429fc332304818ed7d1a9ffe8
Patrick Odenborg <patrick.odenborg@pearson.com>
2019-12-21 13:10:57 -0600

    Create validators for new products page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const { requireTitle, requirePrice } = require('./validators');
+
 const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
@@ -8,7 +12,14 @@ router.get('/admin/products', (req, res) => {
 });
 
 router.get('/admin/products/new', (req, res) => {
-
+  res.send(productsNewTemplate({}));
 });
+
+router.post('/admin/products/new', [ requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  
+  res.send('Submitted.');
+})
 
 module.exports = router;
